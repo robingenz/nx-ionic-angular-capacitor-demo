80f65753d61756b9ee6dccad82d65627d9279cfb
Robin Genz <mail@robingenz.dev>
2025-11-25 14:48:23 +0100

    Fix Capacitor web dir

--- apps/my-app/capacitor.config.ts
+++ apps/my-app/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'my-app',
-  webDir: '../../dist/apps/my-app',
+  webDir: '../../dist/apps/my-app/browser',
   bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
